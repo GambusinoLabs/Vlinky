7315cedf868671b9b7d1fae4da5fe0bb37be4e40
Miguel Medina Ballesteros <maximetinu@gmail.com>
2018-06-04 22:30:18 +0200

    nivelando

--- Assets/Scripts/Generador.cs
+++ Assets/Scripts/Generador.cs
@@ -3,117 +3,35 @@ using System.Collections;
 // Se encarga de generar gelatinas cada tiempo determinado en una posición aleatoria de un rango determinado.
 public class Generador : MonoBehaviour
 {
+    public GameObject prefabASpawnear;         // Determina el primer tipo de objeto a instanciar.
+    public float amplitudSpawn = 7.35f;            // Determinará el rango de distancia en ambas direcciones en el que se generarán los objetos.
 
-    // fields
-    float secondsCounter = 0;
-    float secondsCounter2 = 0;
-    float secondsCounter3 = 0;
-    float secondsToCount;
-    float secondsToCount2;
-    float secondsToCount3;
-    int number = 0;
-    Vector3 rangoGenerador;             // Determinará la posición base en la que se generarán los objetos.
+    [Space(20)]
+    public float esperaInicial = 5.0f;
+    public float tiempoEntreSpawn = 5.0f;
+    public float tiempoRandom = 2.0f;
 
-    // properties
-    public AnimationCurve curva;        // Determina cada cuanto tiempo se genera un nuevo objeto del primer tipo. 
-    public AnimationCurve curva2;       // Determina cada cuanto tiempo se genera un nuevo objeto del segundo tipo.
-    public AnimationCurve curva3;       // Determina cada cuanto tiempo se genera un nuevo objeto del tercer tipo.
-    public GameObject gelatina;         // Determina el primer tipo de objeto a instanciar.
-    public GameObject gelatina2;        // Determina el segundo tipo de objeto a instanciar.
-    public GameObject gelatina3;        // Determina el tercer tipo de objeto a instanciar.
-    public float variable = 7.5f;            // Determinará el rango de distancia en ambas direcciones en el que se generarán los objetos.
+    [Space(20)]
+    public float factorDeVelocidad = 1.0f;
+    public float factorDeVelocidadRandom = 0.1f;
 
-    // methods
+    private Transform dynamicTransform;
+
     void Start()
     {
-
-        // Provoca que se genere un primer objeto nada más iniciar la escena.
-        GenerarGelatina();
-
+        dynamicTransform = GameObject.FindGameObjectWithTag("Dynamic").transform;
+        Invoke("Spawn", esperaInicial);
     }
 
-    void Update()
+    private void Spawn()
     {
-
-        secondsToCount = curva.Evaluate(Time.timeSinceLevelLoad);
-        secondsToCount2 = curva2.Evaluate(Time.timeSinceLevelLoad);
-        secondsToCount3 = curva3.Evaluate(Time.timeSinceLevelLoad);
-        secondsCounter += Time.deltaTime;
-        // Si ha pasado tanto tiempo como indica la variable secondsToCount, se genera una gelatina.
-        if (secondsCounter >= secondsToCount)
-        {
-            secondsCounter = 0;
-            number++;
-            GenerarGelatina();
-        }
-
-        secondsCounter2 += (Time.deltaTime / 2);
-        // Si ha pasado tanto tiempo como indica la variable secondsToCount2, se genera una gelatina.
-        if ((Time.time >= 60f) && (secondsCounter2 >= secondsToCount2))
-        {
-            secondsCounter2 = 0;
-            number++;
-            GenerarGelatina2();
-        }
-
-        secondsCounter3 += Time.deltaTime;
-        if ((Time.time >= 120f) && secondsCounter3 >= secondsToCount3)
-        {
-            secondsCounter3 = 0;
-            number++;
-            GenerarGelatina3();
-        }
-
+        GameObject newGelatine = Instantiate(prefabASpawnear, dynamicTransform);
+        newGelatine.transform.position = transform.position + Random.Range(-amplitudSpawn, +amplitudSpawn) * Vector3.right;
+        GelatinaForce gelatinaSpeedController;
+        gelatinaSpeedController = newGelatine.GetComponentInChildren<GelatinaForce>();
+        if (gelatinaSpeedController == null)
+            gelatinaSpeedController = newGelatine.GetComponent<GelatinaForce>();
+        gelatinaSpeedController.velocidad *= Random.Range(factorDeVelocidad - factorDeVelocidadRandom, factorDeVelocidad + factorDeVelocidadRandom);
+        Invoke("Spawn", Random.Range(tiempoEntreSpawn - tiempoRandom, tiempoEntreSpawn + tiempoRandom));
     }
-
-    void GenerarGelatina()
-    {
-
-        // Determina un rango aleatorio, partiendo de la posición del objeto que lleva este script, añadiendole una distancia igual a variable en cada dirección.
-        rangoGenerador = new Vector3(Random.Range(transform.position.x - variable, transform.position.x + variable), transform.position.y, transform.position.z);
-        // Si hay un objeto asignado en el inspector, se genera en la posicion determinada anteriormente, si no, el Debugger nos informa de que no está asignado.
-        if (gelatina != null)
-        {
-            Instantiate(gelatina, rangoGenerador, transform.rotation);
-        }
-        else
-        {
-            Debug.Log("GenerarGelatina: ¡No has asignado el prefab en el inspector! (O eso parece, porque el GameObject gelatina es = null)");
-        }
-
-    }
-
-    void GenerarGelatina2()
-    {
-
-        // Determina un rango aleatorio, partiendo de la posición del objeto que lleva este script, añadiendole una distancia igual a variable en cada dirección.
-        rangoGenerador = new Vector3(Random.Range(transform.position.x - variable, transform.position.x + variable), transform.position.y, transform.position.z);
-        // Si hay un objeto asignado en el inspector, se genera en la posicion determinada anteriormente, si no, el Debugger nos informa de que no está asignado.
-        if (gelatina2 != null)
-        {
-            Instantiate(gelatina2, rangoGenerador, transform.rotation);
-        }
-        else
-        {
-            Debug.Log("GenerarGelatina: ¡No has asignado el prefab en el inspector! (O eso parece, porque el GameObject gelatina es = null)");
-        }
-    }
-
-    void GenerarGelatina3()
-    {
-
-        // Determina un rango aleatorio, partiendo de la posición del objeto que lleva este script, añadiendole una distancia igual a variable en cada dirección.
-        rangoGenerador = new Vector3(Random.Range(transform.position.x - variable, transform.position.x + variable), transform.position.y, transform.position.z);
-        // Si hay un objeto asignado en el inspector, se genera en la posicion determinada anteriormente, si no, el Debugger nos informa de que no está asignado.
-        if (gelatina3 != null)
-        {
-            Instantiate(gelatina3, rangoGenerador, transform.rotation);
-        }
-        else
-        {
-            Debug.Log("GenerarGelatina: ¡No has asignado el prefab en el inspector! (O eso parece, porque el GameObject gelatina es = null)");
-        }
-
-    }
-
 }
--- Assets/Scripts/TimeManage.cs
+++ Assets/Scripts/TimeManage.cs
@@ -2,63 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 // Se encarga de controlar el sistema de tiempo del juego, acelerándolo o ralentizándolo.
-public class TimeManage : MonoBehaviour 
+public class TimeManage : MonoBehaviour
 {
-	
-	// fields
-	bool pausado = false;						// Define si el juego está pausado o no.
-	float multiplo = 1;
-	float velocidad; 							// Se usa para guardar la velocidad cuando se pausa el juego.
 
-	// properties
-	public AnimationCurve curvaVelocidad;		// Controla el aumento progresivo de velocidad.
+    // fields
+    bool pausado = false;                       // Define si el juego está pausado o no.
+    float multiplo = 1;
+    float velocidad;                            // Se usa para guardar la velocidad cuando se pausa el juego.
 
-	// methods
-	void Update() 
-	{
-		
-		// Provoca que la velocidad del tiempo aumente gradualmente al avanzar la escena en función a una curva del editor. 
-		if (pausado == false) 
-		{
-			//Time.timeScale = (1 + curvaVelocidad.Evaluate(Time.timeSinceLevelLoad)) * multiplo;
-		}
+    // properties
+    public AnimationCurve curvaVelocidad;       // Controla el aumento progresivo de velocidad.
 
-	}
+    void Start()
+    {
+        Time.timeScale = 5.0f;
+    }
 
-	public void Pause() 
-	{
-		
-		// Provoca que el tiempo en la escena se detenga y se guarde la velocidad que tenia antes de la pausa.
-		pausado = true;
-		velocidad = Time.timeScale;
-		Time.timeScale = 0;
+    // methods
+    void Update()
+    {
 
-	}
+        // Provoca que la velocidad del tiempo aumente gradualmente al avanzar la escena en función a una curva del editor. 
+        if (pausado == false)
+        {
+            //Time.timeScale = (1 + curvaVelocidad.Evaluate(Time.timeSinceLevelLoad)) * multiplo;
+        }
+
+    }
+
+    public void Pause()
+    {
+
+        // Provoca que el tiempo en la escena se detenga y se guarde la velocidad que tenia antes de la pausa.
+        pausado = true;
+        velocidad = Time.timeScale;
+        Time.timeScale = 0;
+
+    }
 
 
-	public void Resume() 
-	{
-		
-		// Provoca que la velocidad del tiempo en la escena vuelva a su estado antes de pausarla.
-		Time.timeScale = velocidad;
-		pausado = false;
+    public void Resume()
+    {
 
-	}
-		
-	public void Double() 
-	{
-		
-		// Provoca que la velocidad del tiempo en la escena pase a ser el doble de la actual.
-		multiplo = multiplo *2;
+        // Provoca que la velocidad del tiempo en la escena vuelva a su estado antes de pausarla.
+        Time.timeScale = velocidad;
+        pausado = false;
 
-	}
+    }
 
-	public void Half() 
-	{
-		
-		// Provoca que la velocidad del tiempo en la escena pase a ser la mitad de la actual.
-		multiplo = multiplo / 2;
+    public void Double()
+    {
 
-	}
-		
+        // Provoca que la velocidad del tiempo en la escena pase a ser el doble de la actual.
+        multiplo = multiplo * 2;
+
+    }
+
+    public void Half()
+    {
+
+        // Provoca que la velocidad del tiempo en la escena pase a ser la mitad de la actual.
+        multiplo = multiplo / 2;
+
+    }
+
 }
